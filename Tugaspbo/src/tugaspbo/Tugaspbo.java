219f4fb137b92560638bbf6691aca1ea737b2e46
adyatma1 <134781489+adyatma1@users.noreply.github.com>
2025-01-17 08:54:59 +0800

    Add files via upload

---
+++ Tugaspbo/src/tugaspbo/Tugaspbo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaspbo;
+
+import crudDB.crudDB;
+import javax.swing.JOptionPane;
+import gui.frameUtama;
+
+/**
+ *
+ * @author adyat
+ */
+public class Tugaspbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frameUtama().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
